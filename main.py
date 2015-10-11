a2e2364eeeb6616cdea08cb9c509c02e011b4fed
mrhs <mrhsce@gmail.com>
2015-10-11 10:05:59 +0330

    Input views document added

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+def main():
+    
+    #Input data
+    "در این قسمت باید از طریق یک اسپینر و یا هر ویو دیگری که صلاح میدانی این دیتا ها از کاربر گرفته شود "
+    customerInterArrivalTime = list()  
+    "توزیع آماری زمان رسیدن مشتری جدید"
+    ableServiceTime = list() 
+    "توزیع آماری زمان سرویس دهی ابل"
+    bakerServiceTime = list() 
+    "توزیع آماری زمان سرویس دهی بیکر"
+    ableBakerPriority = 0 #If 0 => Able is first if 1 => baker is first if 2 => randomly chosen
+    "این قسمت یک بولین است که از کاربر پرسیده می شود آیا می خواهد زمان بدهد و یا تعداد کل مشتری ها"
+    
+    selectCustomerOrTime = True #If true customer number is given if false time period is given
+    
+    customerCount = 0  
+    "تعداد کل مشتری ها"
+    timeLength = 0 
+    "زمان کل شبیه سازی"
+    
+    
+    
+    #Data processing 
+    
+    
+    # printing output using django
+    
+    
+    return 0
+
+main()
